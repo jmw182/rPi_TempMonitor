2295515614f9191cc2fa2f39f4ed51dcdaea7385
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2020-01-14 23:24:50 -0500

    Added wrapper class to access Si7021 sensor data

---
+++ src/Si7021EnvSensor.py
@@ -0,0 +1,20 @@
+import board
+import busio
+import adafruit_si7021
+
+# Si7021 Temperature & Humidity Environmetal Sensor
+class EnvSensor:
+
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_si7021.SI7021(i2c)
+
+    def getTempCelsius(self):
+        return self.sensor.temperature
+
+    def getTempF(self):
+        return self.getTempCelsius * 9.0/5.0 + 32
+    
+    def getHumidity(self):
+        return self.sensor.relative_humidity
+
