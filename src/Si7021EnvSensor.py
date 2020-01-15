c867e7160c172f34c8cd7ddce5df1be20e0368bc
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2020-01-14 23:33:31 -0500

    Fixed bugs in __init__ and getTempF()

--- src/Si7021EnvSensor.py
+++ src/Si7021EnvSensor.py
@@ -7,14 +7,14 @@ class EnvSensor:
 
     def __init__(self):
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_si7021.SI7021(i2c)
+        self.sensor = adafruit_si7021.SI7021(self.i2c)
 
     def getTempCelsius(self):
         return self.sensor.temperature
 
     def getTempF(self):
-        return self.getTempCelsius * 9.0/5.0 + 32
-    
+        return self.getTempCelsius() * 9.0/5.0 + 32.0
+
     def getHumidity(self):
         return self.sensor.relative_humidity
 
