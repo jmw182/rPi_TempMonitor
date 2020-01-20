785152c8606e4407e96c27abc9c6e0336490ca53
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2020-01-20 00:46:41 -0500

    Added support for images embedded in html messages in EAS class

--- src/EmailAlertSender.py
+++ src/EmailAlertSender.py
@@ -1,4 +1,8 @@
 import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+import os
 
 class EAS:
 # Wrapper for smtplib to send easy email alerts.
@@ -17,6 +21,7 @@ class EAS:
         self.subject = None
         self.body = None
         self.full_email_text = None
+        self.images = None
 
         self.__password = None
     # end __init__
@@ -57,16 +62,78 @@ class EAS:
         self.server.login(self.sender,self.__password)
     # end login_server
     
-    def form_alert_message(self,subject = None,body = None,recipient = None):
+    def form_alert_message(self,subject = None,body = None,recipient = None,images = None):
         if subject is not None:
             self.subject = subject
         if body is not None:
             self.body = body
         if recipient is not None:
             self.recipient = recipient
+        if images is not None:
+            self.images = images
 
-        self.full_email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s\n" % (self.sender, self.recipient, self.subject, self.body)
+        if self.images is not None:
+            self.form_mime_alert_message(subject,body,recipient)
+        else:
+            self.full_email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s\n" % (self.sender, self.recipient, self.subject, self.body)
+
     # end form_alert_message
+
+    def form_mime_alert_message(self,subject = None,body = None,recipient = None,images = None):
+        if subject is not None:
+            self.subject = subject
+        if body is not None:
+            self.body = body
+        if recipient is not None:
+            self.recipient = recipient
+        if images is not None:
+            self.images = images
+
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = self.subject
+        msgRoot['From'] = self.sender
+        msgRoot['To'] = self.recipient
+        #msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+        # Encapsulate the plain and HTML versions of the message body in an
+        # 'alternative' part, so message agents can decide which they want to display.
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgText = MIMEText(self.body)
+        msgAlternative.attach(msgText)
+
+        # We reference the image in the IMG SRC attribute by the ID we give it below
+        #msgText = MIMEText('<b>Some <i>HTML</i> text</b> and an image.<br><img src="cid:image1"><br>Nifty!', 'html')
+        #msgAlternative.attach(msgText)
+        msgTxt = self.body
+        msgTxt = msgTxt.replace("\n","<br>")
+
+        msgImages = []
+        for img in self.images:
+            im_name = os.path.splitext(os.path.basename(img))[0] # strip path and extension from name
+            fp = open(img,'rb')
+            msgImage = MIMEImage(fp.read())
+            fp.close()
+            msgTxt = msgTxt + '<br><img src="cid:' + im_name + '"><br>' # add image tag to html
+            msgImage.add_header('Content-ID', '<' + im_name + '>')
+            msgImages.append(msgImage)
+            
+        msgText = MIMEText(msgTxt, 'html')
+        msgAlternative.attach(msgText)
+
+        for img in msgImages:
+            msgRoot.attach(img)
+
+        self.full_email_text = msgRoot.as_string()
+        # This example assumes the image is in the current directory
+        #fp = open('test.jpg', 'rb')
+        #msgImage = MIMEImage(fp.read())
+        #fp.close()
+
+        # Define the image's ID as referenced above
+        #msgImage.add_header('Content-ID', '<image1>')
+        #msgRoot.attach(msgImage)
 
     def send_mail(self):
          self.server.sendmail(self.sender,self.recipient,self.full_email_text)
